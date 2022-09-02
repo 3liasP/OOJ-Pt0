a080321b4dd06871c9a4bdc2d05a18596bd1ecce
Jari-Matti Mäkelä <jmjmak@utu.fi>
2022-09-02 07:49:22 +0300

    -

---
+++ src/main/java/fi/utu/tech/ooj/exercise0/Main.java
@@ -0,0 +1,78 @@
+package fi.utu.tech.ooj.exercise0;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Taulukot, joita voi käyttää mikäli ei teen kaikkia tehtäviä. Ohjeet tehtävänannossa.
+     */
+    static int[][] teht4Array = {{1,456,6,-5678,23},{23,22,6,-2,2},{3,1,-5,44,6},{55,3,-8,3,2}};
+    static String[][] teht5Array = {{"1","456","6","5678","23"},{"23","22","6","2","2"},{"3","1","5","44","6"},{"55","3","8","3","2"}};
+
+    /**
+     * Tehtävä 2
+     *
+     * Metodi lukee syötteenä kaksi lukua 1 suurempaa kokonaislukua ja palauttaa ne taulukkona.
+     *
+     * @return Kokonaislukutyyppinen taulukko, jossa on kaksi arvoa 1 suurempaa alkiota
+     */
+
+    // TEE TÄHÄN TEHTÄVÄ 2
+
+    /**
+     * Tehtävä 3
+     *
+     * Metodi lukee syötteenä xSize*ySize kappaletta kokonaislukuja ja sijoittaa ne kokonaislukutyyppiseen
+     * taulukkoon, joka palautetaan metodista.
+     *
+     * @param xSize Taulukon sarakemäärä
+     * @param ySize Taulukon rivimäärä
+     * @return xSize*ySize kokoinen kokonaislukutyyppinen taulukko, joka on täytetty kokonaisluvuilla
+     */
+
+    // TEE TÄHÄN TEHTÄVÄ 3
+
+    /**
+     * Tehtävä 4
+     *
+     * Metodi tarkastaa, että argumenttina saadun taulukon kaikki alkiot ovat positiivisia. Jos jokin luku ei ole
+     * positiivinen, se vaihdetaan itseisarvokseen.
+     *
+     * @param inputArray Kokonaislukutyyppinen taulukko
+     * @return parametrinä saadun taulukon kokoinen merkkijonotyyppinen taulukko
+     */
+
+    // TEE TÄHÄN TEHTÄVÄ 4
+
+    /**
+     * Tehtävä 5
+     * Metodi tulostaa argumenttina saamansa taulukon muotoillen sen taulukkomuotoon ja tasaten sarakkeet oikealle.
+     *
+     * @param inputArray Merkkijonotyyppinen taulukko
+     * @return void
+     */
+
+    // TEE TÄHÄN TEHTÄVÄ 5
+
+    /**
+     * Main class.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+          FizzBuzz-luokan runFizzBuzz()-metodilla voi tarkistaa harjoitustyöpohjan toimivuuden omassa koneessa
+          ennen tehtävien tekemistä. Testin jälkeen alla olevan rivin voi joko kommentoida tai poistaa.
+         */
+        FizzBuzz.runFizzBuzz();
+
+        // Tehtävät 2-5
+        // Kirjoita tähän tarvittavat metodikutsu ja muut toiminteet.
+
+        //Tehtävä 6
+        // Kirjoita tähän tarvittavat metodikutsu ja muut toiminteet.
+        // Osa tehtävästä 6 tehdään RajoitettuLista luokkaan
+
+    }
+}
